243982a6abbc17d8bd13b09b5892b75432e098f5
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-05-03 17:45:45 +0500

    First commit.

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\cordova\drugs\platforms\android\ant-build\AndroidManifest.xml : \
+c:\cordova\drugs\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+c:\cordova\drugs\platforms\android\ant-build\classes.dex : \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\BuildConfig.class \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\CordovaApp.class \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\R$attr.class \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\R$drawable.class \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\R$string.class \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\R$xml.class \
+c:\cordova\drugs\platforms\android\ant-build\classes\com\phonegap\drugstore\R.class \
+c:\cordova\drugs\platforms\android\ant-build\dexedLibs\classes-ffc8d41a7e7b43db06933a04427c274c.jar \
